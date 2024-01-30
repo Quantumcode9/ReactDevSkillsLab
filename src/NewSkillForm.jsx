660eca50cdcfd5f2eb9c9f8afeee71fa33212cff
BrianTaylor <bataylor135@gmail.com>
2024-01-30 15:07:50 -0500

    p1 - 4:

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 export default function NewSkillForm({ onAddSkill }) {
         
     const [skill, setSkill] = useState('');
-    const [level, setLevel] = useState('1');
+    const [level, setLevel] = useState(1);
     
     const handleSkillChange = (event) => {
         setSkill(event.target.value);
       };
     
       const handleLevelChange = (event) => {
-        setLevel(event.target.value);
+        setLevel(Number(event.target.value));
       };
     
       const handleSubmit = (event) => {
@@ -18,7 +18,7 @@ export default function NewSkillForm({ onAddSkill }) {
     
         onAddSkill({ name: skill, level });
         setSkill(''); 
-        setLevel('1'); 
+        setLevel(1); 
       };
     
       return (
@@ -31,12 +31,12 @@ export default function NewSkillForm({ onAddSkill }) {
           <label>
             Level
             <select name="level" value={level} onChange={handleLevelChange}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+</select>
           </label>
           <br />
           <button type="submit">ADD SKILL</button>
